8311d96603a885ebacb569e996c82d00ad9f6ab2
Alexey Anipchenko <pyelya@gmail.com>
2024-06-27 22:25:20 +0100

    functions not finished

---
+++ Functions/functions.py
@@ -0,0 +1,19 @@
+# Functions represent a block of code that performs
+# a specific task and that can be reused in other parts of the program.
+# Previous articles have already used functions. In particular, the print() function,
+# which prints some value to the console.
+# Python has many built-in functions and allows you to define your own functions.
+
+# Formal definition of a function:
+# def function_name ([parameters]):
+#  instructions
+
+# Example
+# definition
+def say_hello():
+    print("Hello from my function!")
+# function call
+say_hello()
+# The function is called say_hello(). 
+# It has no parameters and contains one single instruction,
+# which prints the string "Hello from my function!" to the console.
